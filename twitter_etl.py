de8dd3dd6f2c85a5e715302b3b274aea4b41898d
Olusegun Ajose <ajoseolusegun@gmail.com>
2023-04-07 01:43:03 +0200

    Adding different brands to the output

--- twitter_etl.py
+++ twitter_etl.py
@@ -18,75 +18,81 @@ access_token = config.access_token
 access_token_secret = config.access_token_secret
 bear_token = config.bear_token
 
+# Twitter API credentials
+twitter_api_key = "3hFqNeHA1hnonTYTnlxoSgImN"
+twitter_api_secret = "Qvb9p37eH8vupKU0HpgHQDeCoDIDOuFJP4ukQY3Uo9luCzpV4X"
+access_token = "301911063-SBkqkO40wrC2nDHIIdNYEJG2cc6S2mqlLxla1Jvi"
+access_token_secret = "fEs8DQXEwG9H8ryDzpmGVl9M6matBjurelrvyxFTiZ1vh"
+bear_token = "AAAAAAAAAAAAAAAAAAAAAGQamgEAAAAAM%2Bk2nL2cuNRiAbq0VOOx%2B2CPpak%3DWjcYhNTCLjinZd5Yq4vzlozpd0SoDIunNigHfHFzmDhRe3HRHL"
+
+
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Create API object
-# , wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 api = tweepy.API(auth)
 
-# # # Creating an API object
-# tweets = api.user_timeline(screen_name='@elonmusk',
-#                            # 200 is the maximum allowed count
-#                            count=200,
-#                            include_rts=False,
-#                            # Necessary to keep full_text
-#                            # otherwise only the first 140 words are extracted
-#                            tweet_mode='extended'
-#                            )
-
-search_words = "alpro"  # activa actimel danette"
+search_words = ["alpro", "activa", "actimel", "danette"]
 date_since = "2023-01-01"
-
-# # search tweets with the search_words and a limit of 200 tweets
-tweets = api.search_tweets(q=search_words, lang="en",
-                           since=date_since, count=200)
-
-# api.search_tweets(q=search_words, lang="en", since=date_since)
-# print(tweets[0])
-# Collect tweets
-# tweets = tweepy.Cursor(api.search_tweets, q=search_words,
-#                        lang="en", since=date_since).items(200)
-
-print(f"Got {len(tweets)} tweets")
-
 refined_tweets_list = []
-for tweet in tweets:
-    text = tweet._json["text"]
 
-    refined_tweet = {"user": tweet.user.screen_name,
-                     'text': text,
-                     'sentiment': classifier(text)[0]['label'],
-                     'favorite_count': tweet.favorite_count,
-                     'retweet_count': tweet.retweet_count,
-                     'created_at': tweet.created_at,
-                     'link': f'https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}'}
+for search_word in search_words:
+    # # search tweets with the search_words and a limit of 200 tweets
+    tweets = api.search_tweets(q=search_word, lang="en", tweet_mode="extended",
+                               since=date_since, count=200)
 
-    refined_tweets_list.append(refined_tweet)
+    if not tweets:
+        print("No tweets found")
+        continue
+    else:
+        print(f"Got {len(tweets)} tweets for  {search_word}")
+
+    for tweet in tweets:
+        text = tweet.full_text
+
+        refined_tweet = {"product": search_word,
+                         "user": tweet.user.screen_name,
+                         'text': text,
+                         'sentiment': classifier(text)[0]['label'],
+                         'favorite_count': tweet.favorite_count,
+                         'retweet_count': tweet.retweet_count,
+                         'created_at': tweet.created_at,
+                         'link': f'https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}'}
+
+        refined_tweets_list.append(refined_tweet)
 
 df = pd.DataFrame(refined_tweets_list)
 df['created_at'] = pd.to_datetime(df['created_at'])
 # convert special characters to ascii
 df['text'] = df['text'].str.encode('ascii', 'ignore').str.decode('ascii')
 df.to_csv('refined_tweets.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
+# df.to_excel(f'refined_tweets_{search_word}.xlsx', index=False)
 
-# Create a tweet list as follows:
-# tweets = []
 
-# # Define the search term and the date_since date as variables
-# search_words = "#covid19"
-# date_since = "2020-01-01"
+# # # search tweets with the search_words and a limit of 200 tweets
+# tweets = api.search_tweets(q=search_words, lang="en", tweet_mode="extended",
+#                            since=date_since, count=100)
 
-# # Collect tweets
-# tweets = tweepy.Cursor(api.search, q=search_words,
-#                        lang="en", since=date_since).items(1000)
+# print("No tweets found") if not tweets else print(f"Got {len(tweets)} tweets")
 
-# # Iterate and print tweets
+# refined_tweets_list = []
 # for tweet in tweets:
-#     print(tweet.text)
+#     text = tweet.full_text
 
-# # Iterate and store tweets in a list
-# tweets = []
-# for tweet in tweepy.Cursor(api.search, q=search_words, lang="en", since=date_since).items(1000):
-#     tweets.append(tweet)
+#     refined_tweet = {"user": tweet.user.screen_name,
+#                      'text': text,
+#                      'sentiment': classifier(text)[0]['label'],
+#                      'favorite_count': tweet.favorite_count,
+#                      'retweet_count': tweet.retweet_count,
+#                      'created_at': tweet.created_at,
+#                      'link': f'https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}'}
+
+#     refined_tweets_list.append(refined_tweet)
+
+# df = pd.DataFrame(refined_tweets_list)
+# df['created_at'] = pd.to_datetime(df['created_at'])
+# # convert special characters to ascii
+# df['text'] = df['text'].str.encode('ascii', 'ignore').str.decode('ascii')
+# # df.to_csv('refined_tweets.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
+# df.to_excel('refined_tweets.xlsx', index=False)
